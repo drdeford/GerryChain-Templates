8649ca729bfeaf887f676edd84a006f319a549c0
Daryl DeFord <daryl.r.deford@gmail.com>
2019-06-04 15:57:15 -0400

    PA files

--- Pennsylvania/PA_Chain.py
+++ Pennsylvania/PA_Chain.py
@@ -7,6 +7,9 @@ import seaborn as sns
 import json
 import networkx as nx
 import time 
+import os
+import csv
+
 
 from gerrychain import Graph, Partition, Election
 from gerrychain.updaters import Tally, cut_edges
@@ -17,7 +20,7 @@ from gerrychain.accept import always_accept
 from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain,
                         proposals, updaters, constraints, accept, Election)
 from gerrychain.constraints.validity import within_percent_of_ideal_population
-
+from gerrychain.metrics import mean_median,efficiency_gap
 from gerrychain.proposals import recom
 from functools import partial
 
@@ -28,16 +31,16 @@ with open(newdir + "init.txt", "w") as f:
     
 
 graph_path =  "./Data/PA_VTDALL.json"#"./Data/PA_BPOP_FINAL/VTD_FINAL.shp"
-plot_path = "./Data/VTD_FINAL.shp
+plot_path = "./Data/VTD_FINAL.shp"
 
 
-df = gp.read_file(plot_path) 
+df = gpd.read_file(plot_path) 
 
 
 def num_splits(partition):
 
 
-    df["current"] = df[unique_label].map(partition.assignment)
+    df["current"] = df[unique_label].map(dict(partition.assignment))
     
 
     splits = sum(df.groupby('COUNTYFP10')['current'].nunique() >1)
@@ -68,9 +71,11 @@ graph = Graph.from_json(graph_path)
 updaters = {"population": updaters.Tally("TOT_POP", alias="population"),
             "cut_edges": cut_edges}
             
-            
-for i in range(num_elections):
-    updaters.update(Election(election_names[i],{"Democratic":election_columns[i][0],"Republican":election_columns[i][1]}))
+elections = [Election(election_names[i],{"Democratic":election_columns[i][0], "Republican":election_columns[i][1]}) for i in range(num_elections)]
+ 
+election_updaters = {election.name: election for election in elections}          
+
+updaters.update(election_updaters)
     
 
 initial_partition = Partition(graph, "2011_PLA_1", updaters)
@@ -85,11 +90,15 @@ ideal_population = sum(initial_partition["population"].values()) / len(initial_p
 proposal = partial(recom,
                        pop_col="TOT_POP",
                        pop_target=ideal_population,
-                       epsilon=0.05,
+                       epsilon=0.02,
                        node_repeats=2
                       )
 
-
+compactness_bound = constraints.UpperBound(
+        lambda p: len(p["cut_edges"]),
+        2*len(initial_partition["cut_edges"])
+    )
+    
 chain = MarkovChain(
 proposal=proposal, 
 constraints=[
@@ -98,10 +107,117 @@ constraints=[
 ],
 accept=accept.always_accept,
 initial_state=initial_partition,
-total_steps=10000
+total_steps=1000
     )
     
     
+
+with open(newdir + "Start_Values.txt", "w") as f:
+    f.write("Values for Starting Plan: 2011 Enacted\n \n ")
+    f.write("Initial Cut: "+ str(len(initial_partition["cut_edges"])))
+    f.write("\n")
+    f.write("\n")
+    f.write("Initial County Splits: "+ str(num_splits(initial_partition)))
+    f.write("\n")
+    f.write("\n")
+
+    for elect in range(num_elections):
+        f.write(election_names[elect] + "District Percentages" + str(sorted(initial_partition[election_names[elect]].percents("Democratic"))))
+        f.write("\n")
+        f.write("\n")
+
+        f.write(election_names[elect] + "Mean-Median :"+ str(mean_median(initial_partition[election_names[elect]])))
+        
+        f.write("\n")
+        f.write("\n")
+        
+        f.write(election_names[elect] + "Efficiency Gap :" + str(efficiency_gap(initial_partition[election_names[elect]])))
+        
+        f.write("\n")
+        f.write("\n")
+        
+        f.write(election_names[elect] + "How Many Seats :" + str(initial_partition[election_names[elect]].wins("Democratic")))
+         
+        f.write("\n")
+        f.write("\n")
+        
+    
+
+pop_vec=[]
+cut_vec=[]
+votes=[[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+mms=[]
+egs=[]
+hmss=[]
+
+splits =[]
+
+t=0
 for part in chain:
-    pass
+    splits.append(num_splits(part))
+    
+    pop_vec.append(sorted(list(part["population"].values())))
+    cut_vec.append(len(part["cut_edges"]))
+    mms.append([])
+    egs.append([])
+    hmss.append([])
+    
+    for elect in range(num_elections):
+        votes[elect].append(sorted(part[election_names[elect]].percents("Democratic")))
+        mms[-1].append(mean_median(part[election_names[elect]]))
+        egs[-1].append(efficiency_gap(part[election_names[elect]]))
+        hmss[-1].append(part[election_names[elect]].wins("Democratic"))
+        
+        
+    t+=1
+    if t%100==0:
+        print(t)
+        with open(newdir+"mms"+str(t)+".csv",'w') as tf1:
+            writer = csv.writer(tf1,lineterminator="\n")
+            writer.writerows(mms)
+			
+        with open(newdir+"egs"+str(t)+".csv",'w') as tf1:
+            writer = csv.writer(tf1,lineterminator="\n")
+            writer.writerows(egs)
+			
+        with open(newdir+"hmss"+str(t)+".csv",'w') as tf1:
+            writer = csv.writer(tf1,lineterminator="\n")
+            writer.writerows(hmss)
+			
+        with open(newdir+"pop"+str(t)+".csv",'w') as tf1:
+            writer = csv.writer(tf1,lineterminator="\n")
+            writer.writerows(pop_vec)
+	
+        with open(newdir+"cuts"+str(t)+".csv",'w') as tf1:
+            writer = csv.writer(tf1,lineterminator="\n")
+            writer.writerows([cut_vec])
+
+        with open(newdir+"splits"+str(t)+".csv",'w') as tf1:
+            writer = csv.writer(tf1,lineterminator="\n")
+            writer.writerows([splits])      
+
+
+        with open(newdir+"assignment"+str(t)+".json", 'w') as jf1:
+            json.dump(dict(part.assignment), jf1)
+			
+			
+        for elect in range(num_elections):
+            with open(newdir+election_names[elect]+"_"+str(t)+".csv",'w') as tf1:
+                writer = csv.writer(tf1,lineterminator="\n")
+                writer.writerows(votes[elect])
+
+        df["plot"+str(t)]=df["GEOID10"].map(dict(part.assignment))
+        df.plot(column="plot"+str(t),cmap="tab20")
+        plt.savefig(newdir+"plot"+str(t)+".png")
+        plt.close()		    
+
+        votes=[[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+        mms=[]
+        egs=[]
+        hmss=[]
+        pop_vec=[]
+        cut_vec=[]
+        splits =[]
+
+    
 
